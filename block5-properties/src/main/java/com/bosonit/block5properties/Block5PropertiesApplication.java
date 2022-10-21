5b7a487ea82f3c5cd91577e507240efe77230fde
joseantoniomorales-bosonit <joseantonio.morales@bosonit.com>
2022-10-21 11:22:58 +0200

    imprimiendo propierties

--- block5-properties/src/main/java/com/bosonit/block5properties/Block5PropertiesApplication.java
+++ block5-properties/src/main/java/com/bosonit/block5properties/Block5PropertiesApplication.java
@@ -2,6 +2,7 @@ package com.bosonit.block5properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,12 +11,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Block5PropertiesApplication implements CommandLineRunner {
 	private static final Logger LOG = LoggerFactory.getLogger(Block5PropertiesApplication.class);
 
+	@Value("${greeting}")
+	private String greeting;
+	@Value("${new.property:no tiene valor}")
+	private String newProperty;
+	@Value("${my.number}")
+	private int myNumber;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Block5PropertiesApplication.class, args);
 	}
 
 	public void run(String[] args){
-		LOG.info("Hola mundo");
+		LOG.info("El valor de greeting es: "+this.greeting);
+		LOG.info("El valor de my.number es: "+this.myNumber);
+		LOG.info("El valor de new.property es: "+this.newProperty);
 	}
 
 }
