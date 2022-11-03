bfec91277e16575a2cfd002450b2a87ce06e0cc2
joseantoniomorales-bosonit <joseantonio.morales@bosonit.com>
2022-11-03 14:07:22 +0100

    Terminado

--- ejercicioCompetitivo2/src/main/java/com/bosonit/ejercicioCompetitivo2/controllers/PrincipalController.java
+++ ejercicioCompetitivo2/src/main/java/com/bosonit/ejercicioCompetitivo2/controllers/PrincipalController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -14,18 +16,26 @@ import java.util.stream.Collectors;
 public class PrincipalController {
 
     @GetMapping(value={"/**"})
-    public ControllerObject entryOther(HttpServletRequest request){
+    public String entryOther(HttpServletRequest request, HttpServletResponse resp) throws IOException {
+        if(request.getHeader("REDIRIGE").equals("SALTA")){
+            resp.sendRedirect("/salta");
+        }
         List<String> path = Arrays.stream(request.getRequestURI().split("/")).toList();
         Map<String,String> getMap = request.getParameterMap().entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, entry -> Arrays.toString(entry.getValue())));
         Map<String,String> headers = Collections.list(request.getHeaderNames()).stream().collect(Collectors.toMap(h -> h, request::getHeader));
         String urlOrigen = request.getRemoteAddr();
 
         ControllerObject controllerObject = new ControllerObject(path,getMap,headers,urlOrigen);
-        return controllerObject;
+        return controllerObject.toString();
     }
 
     @GetMapping(value={"/","one"})
     public String entryOne(){
         return "Entry one";
+    }
+
+    @GetMapping(value={"/salta"})
+    public String entryJump() {
+        return "he ido a Jump";
     }
 }
