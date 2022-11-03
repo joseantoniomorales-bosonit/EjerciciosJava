533d9af72aa88b17e64d487aa77bb1511be5fc67
joseantoniomorales-bosonit <joseantonio.morales@bosonit.com>
2022-11-03 16:52:30 +0100

    Parte del POST

--- ejercicioCompetitivo2/src/main/java/com/bosonit/ejercicioCompetitivo2/controllers/PrincipalController.java
+++ ejercicioCompetitivo2/src/main/java/com/bosonit/ejercicioCompetitivo2/controllers/PrincipalController.java
@@ -1,6 +1,8 @@
 package com.bosonit.ejercicioCompetitivo2.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,11 +15,23 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
-public class PrincipalController {
+public class PrincipalController{
+
+    @PostMapping(value={"/**"})
+    public ResponseEntity<String> entryOtherPost (HttpServletRequest request, HttpServletResponse response){
+        if(request.getHeader("ADD") != null){
+            return ResponseEntity.ok().body("MODIFICADO");
+        }
+        return (ResponseEntity<String>) ResponseEntity.ok();
+    }
 
     @GetMapping(value={"/**"})
     public ControllerObject entryOther(HttpServletRequest request, HttpServletResponse resp) throws IOException {
-        if( request.getHeader("REDIRIGE") != null && request.getHeader("REDIRIGE").equals("SALTA")){
+        resp.addHeader("hola","");
+        if(request.getHeader("REDIRIGE") != null && request.getHeader("REDIRIGE").equals("SALTA")){
+
+
+            System.out.println(Collections.list(request.getHeaderNames()).stream().collect(Collectors.toMap(h -> h, request::getHeader)));
             resp.sendRedirect("/salta");
         }
         List<String> path = Arrays.stream(request.getRequestURI().split("/")).toList();
